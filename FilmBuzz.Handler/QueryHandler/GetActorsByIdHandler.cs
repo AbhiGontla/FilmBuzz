e08872a3906b910e2879eff14cfdc72539f4a664
AbhinayKumar <abhinay3kumar@gmail.com>
2020-05-27 11:25:43 +0530

    Add project files.

---
+++ FilmBuzz.Handler/QueryHandler/GetActorsByIdHandler.cs
@@ -0,0 +1,29 @@
+﻿using FilmBuzz.Common.Interface;
+using FilmBuzz.Common.Model;
+using FilmBuzz.Handler.Query;
+using FilmBuzz.Handler.QueryResult;
+using FilmBuzz.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmBuzz.Handler.QueryHandler
+{
+    public class GetActorsByIdHandler : IQueryHandler<GetActorsById, GetActorsResult>
+    {
+        public GetActorsResult Execute(GetActorsById query)
+        {
+            var user = RepositoryFactory.GetInstance<TBL_ACTORS>().GetAllWith(filter => filter.ActorId == query.ActorId).FirstOrDefault();
+            return new GetActorsResult()
+            {
+                Name=user.Name,
+                Sex=user.Sex,
+                Bio=user.Bio,
+                DOB=user.DOB,
+                ActorId=user.ActorId
+            };
+        }
+    }
+}
---
+++ FilmBuzz.Repository/Repository/MovieRepository.cs
@@ -0,0 +1,78 @@
+﻿using FilmBuzz.Common.Interface;
+using FilmBuzz.Common.Model;
+using FilmBuzz.Repository.DbContext;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmBuzz.Repository.Repository
+{
+    class MovieRepository : IRepository<TBL_MOVIES>
+    {
+        public bool Create(TBL_MOVIES entity)
+        {
+            bool status = false;
+            try
+            {
+                for (int i = 0; i < entity.ActorsId.Length; i++)
+                {
+                    using (var _dbContext = new FilmBuzzDbContext())
+                    {
+
+                        _dbContext.TBL_MOVIES.Add(new TBL_MOVIES()
+                        {
+                            Name = entity.Name,
+                            ActorId = entity.ActorsId[i],
+                            Plot = entity.Plot,
+                            ProducerId = entity.ProducerId,
+                            YearofRelease = entity.YearofRelease
+                        });
+                        status= _dbContext.SaveChanges() == 1 ? true : false;
+                        
+
+                    }
+                }
+                return status;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(TBL_MOVIES entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TBL_MOVIES> GetAll()
+        {
+            using (var _dbContext = new FilmBuzzDbContext())
+            {
+                return _dbContext.TBL_MOVIES.ToListAsync().Result;
+            }
+        }
+
+        public IEnumerable<TBL_MOVIES> GetAllWith(object param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TBL_MOVIES> GetAllWith(Expression<Func<TBL_MOVIES, bool>> Expression)
+        {
+            using (var _dbContext = new FilmBuzzDbContext())
+            {
+                return _dbContext.TBL_MOVIES.Where(Expression).ToListAsync().Result;
+            }
+        }
+
+        public bool Update(TBL_MOVIES entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
